0ebb229e1d28158c1a298de6206165a91d7e3058
Rick <fishorb@gmail.com>
2016-12-22 19:27:47 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.use('', function (req, res) {
+
+})
+
+app.listen(8080, function () {
+  console.log('app listening on port 8080!')
+})
